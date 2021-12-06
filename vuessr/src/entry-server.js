f752c252938d8dd64d513b5115123bcd4c1abaeb
qiuzhi <qiuzhi@youzan.com>
2021-12-06 18:42:50 +0800

    1

--- vuessr/src/entry-server.js
+++ vuessr/src/entry-server.js
@@ -2,15 +2,31 @@ import { createApp } from './app';
 
 export default context => {
     return new Promise((resolve, reject) => {
+
         const { app, router } = createApp();
         router.push(context.url);
+
         router.onReady(()=> {
+
             const matchedComponents = router.getMatchedComponents();
+
             if (!matchedComponents.length) {
                 return reject({ code: 404 });
             }
-            // 正常的情况
-            resolve(app);
+
+            Promise.all(matchedComponents.map(Component => {
+                if (Component.asyncData) {
+                    return Component.asyncData({
+                        store,
+                        route: router.currentRoute
+                      })
+                }
+            })).then(_ => {
+                context.state = store.state
+                resolve(app);
+            }).catch(reject);
+            
         }, reject)
+
     }).catch(new Function());
 }
